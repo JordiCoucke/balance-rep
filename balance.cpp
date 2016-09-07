284c5c2890af952402cf333c8c6c8ae2487b7442
Speronowa <ekcuoc.idroj@gmail.com>
2016-09-07 18:33:22 +0200

    Add files via upload

--- balance.cpp
+++ balance.cpp
@@ -1,7 +1,8 @@
+
 #include <iostream>
 using namespace std;
 
-int balance(int gewicht);
+int balance(int gewicht,int& hoeveelheid);
 
 int main()
 {
@@ -10,13 +11,21 @@ int main()
 		cout<<"Hoeveel opgaven zou je willen?"<<endl;
 		cin>>n;
 	} while (!(n>=1 && n<=250));
+	cout<<"Geef deze een waarde."<<endl;
 	for (n ; n>0;n--)
 	{
-		balance(heavy);
+		balance(heavy,n);
 	}
 }
 
-int balance(int gewicht)
+int balance(int gewicht,int& hoeveelheid)
 {
+	int t=0;
+	for(t;t<=hoeveelheid;t++)
+	{
+		cin>>gewicht;
+		return gewicht;
+	}
 	
 }	
+
