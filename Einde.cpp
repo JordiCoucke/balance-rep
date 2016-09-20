1abff6b5d9665fa6085922456d0bf1f2838f4ac4
Speronowa <ekcuoc.idroj@gmail.com>
2016-09-21 00:11:02 +0200

    Add files via upload

---
+++ Einde.cpp
@@ -0,0 +1,71 @@
+
+
+#include <iostream>
+
+using namespace std;
+#include <vector>
+
+int main()
+{
+	int n,som,m,k,opgaves,opgave;
+	vector<int>v;//vector v declareren of initializeren 
+	cin>>opgaves;
+	int testgewicht[opgaves];
+	for(int o = 0; o<opgaves ;o++)
+	{
+		
+		do	{
+			cin >> n;
+		} while (!(n>=1 && n<=250));
+		m=n;
+		for (n ; n>0;n--)
+		{
+			do	{
+			cin >> k;
+			v.push_back(k);// getallen k toewijzen aan vector
+		} while (!(n>0 && n<=1000));
+		}
+		vector<int> w (v);//aanmaken w die een kopie is van v
+		cin >> testgewicht[o];
+	 	opgave=0;
+	for (vector<int>::iterator i = v.begin(); i != v.end(); ++i) 
+	{		
+		for (vector<int>::iterator j = v.begin(); j != v.end(); ++j) 
+		{
+		if(*i == *j )
+				{	
+				}
+				else if(*i != *j)
+				{
+					som=0;
+					som = *i + *j;
+					//cout<<endl<<som<<endl; DOE DE STREEPJES WEG OM TE ZIEN DAT DE SOM KLOPT!!!
+					if(som == testgewicht[o])
+					{
+						opgave++;
+						
+					}
+					
+				
+				}	
+		}
+		
+	}
+				
+		if(opgave >0)
+						{
+						cout << testgewicht[o] << " JA"  << endl<<endl;
+						}
+		if(opgave <=0)
+						{
+						cout << testgewicht[o] << " NEEN" << endl<<endl;
+						}	
+		
+
+		
+	}
+}
+					
+		
+			
+
